863b6d2e1a38d39501155ed86a68a02abd6d204e
Jedrzej <jk.kuczynski@gmail.com>
2019-04-30 23:16:55 +0200

    Porządki w kodzie i liczenie podlistków

--- features.py
+++ features.py
@@ -1,73 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import skimage
+from skimage import color, io, measure, morphology
 
 
-def img_labels(file, threshold):
-    img = skimage.io.imread(file)
-    img_gray = skimage.color.rgb2gray(img)
+def threshold_and_label(file, threshold):
+    img = io.imread(file)
+    img_gray = color.rgb2gray(img)
     mask = (img_gray < threshold)
-    labels = skimage.measure.label(mask)
+    labels = measure.label(mask)
     return labels
 
 
-# img_labelled_rgb = img_labels("leafsnap-subset1/crataegus_crus-galli/pi0062-07-2.jpg", 0.6)
-# img_labelled_rgb = skimage.color.label2rgb(img_labelled_rgb)
-# skimage.io.imshow(img_labelled_rgb)
-# plt.show()
+def subleaves(leaves, list_of_masks=[]):
+    subleaves_dict = {}
+    for key, value in leaves.items():
+        subleaves_dict[key] = []
+        for img in value:
+            subleaves = []
+            for mask in list_of_masks:
+                eroded_img = morphology.binary_erosion(img, mask)
+                eroded_img_labelled = measure.label(eroded_img)
+                eroded_img_regions = measure.regionprops(eroded_img_labelled)
+                subleaves_number = len(eroded_img_regions)
+                subleaves.append(subleaves_number)
+            subleaves_dict[key].append(subleaves)
+    return subleaves_dict
 
 
-# Areal? BBox? Centroid?
-
-
-counter_all = 0
-counter_found = 0
+leaves_dict = {}
 threshold = 0.7
 
 for root, dirs, files in os.walk("./leafsnap-subset1", topdown=False):
+    species = root.split("/")[-1]
+    if species != "leafsnap-subset1":
+        leaves_dict[species] = []
     for name in files:
-        counter_all += 1
         filepath = os.path.join(root, name)
-        # print(filepath)
-        # print(counter)
-        img_labelled = img_labels(filepath, threshold)
-        region_props = skimage.measure.regionprops(img_labelled)
-        # img_labelled_rgb = skimage.color.label2rgb(img_labelled)
-        # skimage.io.imshow(img_labelled_rgb)
-        # plt.show()
-        counter_leaves = 0
-        # print(img_labelled)
+        img_labelled = threshold_and_label(filepath, threshold)
+        region_props = measure.regionprops(img_labelled)
         width = len(img_labelled[0, :])
         height = len(img_labelled[:, 0])
-        l = []
+        objects_found = []
 
         for region in region_props:
-            bounding_box = region.bbox
-            # print("REGION: ", region.area, bounding_box, region.centroid)
-            if bounding_box[2] < 0.88 * height and bounding_box[3] < 0.88 * width:
-                if bounding_box[0] < 0.6 * height and bounding_box[1] < 0.6 * width:
-                # if 300 < region.centroid[0] < 700 and 300 < region.centroid[1] < 700:
-                    # print(region.centroid, "\t", region.area, "\t", region.bbox)
+            bound_box = region.bbox
+            if bound_box[2] < 0.88 * height and bound_box[3] < 0.88 * width:
+                if bound_box[0] < 0.6 * height and bound_box[1] < 0.6 * width:
                     if region.area >= 4000:
-                        # print("PRZESZLO: ", width, height, region.area, bounding_box, region.centroid)
-                        # img_labelled_rgb = skimage.color.label2rgb(img_labelled)
-                        # skimage.io.imshow(region.image)
-                        # plt.show()
-                        skimage.io.imshow(region.image)
-                        plt.show()
-                        counter_found += 1
-                        counter_leaves += 1
-                        l.append([width, height, region.area, bounding_box, region.centroid])
+                        objects_found.append(region.image)
 
-        if counter_leaves != 1:
-            pass
-            # print(filepath, counter_leaves)
-            # img_labelled_rgb = skimage.color.label2rgb(img_labelled)
-            # skimage.io.imshow(img_labelled_rgb)
-            # plt.show()
+        if len(objects_found) == 1:
+            leaves_dict[species].append(objects_found[0])
 
-print("ALL: ", counter_all, " FOUND: ", counter_found)
 
-# skimage.io.imshow(img_labels)
-# plt.show()
+disk_masks = [morphology.disk(5), morphology.disk(10), morphology.disk(15)]
+subleaves_dict = subleaves(leaves_dict, disk_masks)
