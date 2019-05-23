5c64aca9ae2fd51523431a20d8c9f49a0e1e75ec
Jędrzej Kuczyński <jk.kuczynski@gmail.com>
2019-05-23 20:45:41 +0200

    Wszystkie JJa. Pytanie co z wyborem cech i dalszymi eksperymentami.

--- experiments.py
+++ experiments.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectPercentile
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
 from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -26,18 +30,18 @@ def load_and_prepare_data(filename):
 
 def neural_net_experiments(X, y):
     clf = MLPClassifier(solver="lbfgs", random_state=42)
-    pipe = Pipeline(steps=[("net", clf)])
     param_grid = {
-         "net__hidden_layer_sizes": [(25,), (50,), (100,), (150,), (200,),
-                                     (50, 25), (100, 50),
-                                     (125, 75), (175, 125),
-                                     (25, 15, 5), (50, 30, 15),
-                                     (75, 45, 25), (100, 60, 35)],
-         "net__activation": ["logistic", "tanh", "relu"],
-         "net__alpha": [0.001, 0.0001, 0.00001, 0.15, 0.3, 0.5, 0.7],
-         "net__max_iter": [100, 200, 300, 400, 500, 600, 700, 800]
-         }
-    grid_search = GridSearchCV(pipe, param_grid)  # cv=3, verbose=int
+        "hidden_layer_sizes": [(25,), (50,), (100,), (150,), (200,),
+                               (50, 25), (100, 50),
+                               (125, 75), (175, 125),
+                               (25, 15, 5), (50, 30, 15),
+                               (75, 45, 25), (100, 60, 35)],
+        "activation": ["logistic", "tanh", "relu"],
+        "alpha": [0.001, 0.0001, 0.00001, 0.15, 0.3, 0.5, 0.7],
+        "max_iter": [100, 200, 300, 400, 500, 600, 700, 800]
+        }
+    grid_search = GridSearchCV(clf, param_grid, scoring="accuracy",
+                               cv=3, iid=False)
     grid_search.fit(X, y)
 
     search_results_df = pd.DataFrame.from_dict(grid_search.cv_results_)
@@ -47,9 +51,82 @@ def neural_net_experiments(X, y):
 
     print(best_score, best_params)
 
-    search_results_df.to_csv("neural_net_all_features.csv")
+    search_results_df.to_csv("neural_net_all_features.csv", index=False)
+
+    # Feature Selection
+
+    feature_selection = SelectPercentile(percentile=75)
+    X_selected = feature_selection.fit_transform(X, y)
+    selected_features = feature_selection.get_support()
+
+    print(X.shape, X_selected.shape)
+
+    return
+
+
+def naive_bayes_experiments(X, y):
+    clf = GaussianNB()
+    param_grid = {
+        "var_smoothing": [1e-9, 1e-8, 1e-7, 1e-6, 1e-5,
+                          1e-4, 1e-3, 1e-2, 1e-1]
+    }
+    grid_search = GridSearchCV(clf, param_grid, scoring="accuracy",
+                               cv=3, iid=False)
+    grid_search.fit(X, y)
+
+    search_results_df = pd.DataFrame.from_dict(grid_search.cv_results_)
+    best_estimator = grid_search.best_estimator_
+    best_score = grid_search.best_score_
+    best_params = grid_search.best_params_
+
+    print(best_score, best_params)
+
+    search_results_df.to_csv("naive_bayes_all_features.csv", index=False)
+
+    # Feature Selection
+
+    feature_selection = SelectPercentile(percentile=75)
+    X_selected = feature_selection.fit_transform(X, y)
+    selected_features = feature_selection.get_support()
+
+    print(X.shape, X_selected.shape)
+
+    return
+
+
+def gaussian_process_experiments(X, y):
+    kernel = 1.0 * RBF(1.0)
+    clf = GaussianProcessClassifier(kernel=kernel, random_state=42)
+    param_grid = {
+        "n_restarts_optimizer": [0, 1, 2, 3, 4, 5],
+        "max_iter_predict": [100, 200, 300, 400,
+                             500, 600, 700, 800]
+    }
+    grid_search = GridSearchCV(clf, param_grid, scoring="accuracy",
+                               cv=3, iid=False)
+    grid_search.fit(X, y)
+
+    search_results_df = pd.DataFrame.from_dict(grid_search.cv_results_)
+    best_estimator = grid_search.best_estimator_
+    best_score = grid_search.best_score_
+    best_params = grid_search.best_params_
+
+    print(best_score, best_params)
+
+    search_results_df.to_csv("gaussian_process_all_features.csv", index=False)
+
+    # Feature Selection
+
+    feature_selection = SelectPercentile(percentile=75)
+    X_selected = feature_selection.fit_transform(X, y)
+    selected_features = feature_selection.get_support()
+
+    print(X.shape, X_selected.shape)
+
     return
 
 
 features, targets = load_and_prepare_data("test.npz")
-neural_net_experiments(features, targets)
+# neural_net_experiments(features, targets)
+naive_bayes_experiments(features, targets)
+# gaussian_process_experiments(features, targets)
